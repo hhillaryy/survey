299adb8d2b3df27c88ee75569e8f72514e7ab236
Alex Fallenstedt and Hillary Hintzen <hillary07@gmail.com>
2016-01-12 15:43:48 -0800

    add all survey project files

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#user-info form").submit(function(){
+    var fName = $("#firstname").val();
+    var lName = $("#lastname").val();
+    var favFood = $("#food").val();
+    var favMusic = $("#music").val();
+    var favColor = $("#color").val();
+    var favAnimal = $(".animal").val();
+
+    $(".output1").text(fName);
+    $(".output2").text(lName);
+    $(".output3").text(favFood);
+    $(".output4").text(favMusic);
+    $(".output5").text(favColor);
+    $(".output6").text(favAnimal);
+
+    $("#confirm").show();
+    event.preventDefault();
+
+  });
+});
